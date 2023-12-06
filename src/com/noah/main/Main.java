d3554d9ad1a32f6e87022d1ed1ccee36428db914
404kevnotfound <44481502+TweakyTurnip875@users.noreply.github.com>
2023-12-06 10:01:51 -0700

    initial commit

---
+++ src/com/noah/main/Main.java
@@ -0,0 +1,23 @@
+package com.noah.main;
+
+import javafx.application.Application;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		Pane pane = new Pane();
+		
+		Scene scene = new Scene(pane, 300, 300);
+		
+		stage.setTitle("Snake Game");
+		stage.setScene(scene);
+		stage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
