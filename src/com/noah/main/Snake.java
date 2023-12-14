d0e2e3316023555deea9433690e702426399e04d
404kevnotfound <44481502+TweakyTurnip875@users.noreply.github.com>
2023-12-14 09:36:06 -0700

    initialized snake class

--- src/com/noah/main/Snake.java
+++ src/com/noah/main/Snake.java
@@ -1,5 +1,14 @@
 package com.noah.main;
 
-public class Snake {
+import java.util.LinkedList;
 
+public class Snake {
+	LinkedList<Cell> snake = new LinkedList<>();
+	private Cell head;
+	
+	public Snake(Cell startPosition) {
+		head = startPosition;
+		snake.add(head);
+		head.setCellType(CellType.SNAKE_NODE);
+	}
 }
