43d3d8b1dfb847f36f10ad89764c47e00b9abf19
TweakyTurnip875 <44481502+TweakyTurnip875@users.noreply.github.com>
2024-01-08 15:49:23 -0700

    Updated colors.

--- src/com/noah/main/Cell.java
+++ src/com/noah/main/Cell.java
@@ -5,9 +5,9 @@ import javafx.scene.layout.*;
 public class Cell extends Pane {
 	private int column, row;
 	private CellType cellType;
-	private String styleSnakeNode = "-fx-background-color: black;";
-	private String styleEmptyNode = "-fx-background-color: white; -fx-border-color:black;";
-	private String styleFoodNode = "-fx-background-color: red;";
+	private String styleSnakeNode = "-fx-background-color: #F5F5DC;";
+	private String styleEmptyNode = "-fx-background-color: #202121;";
+	private String styleFoodNode = "-fx-background-color: #a1b0c7;";
 	
 	public Cell(int column, int row) {
 		this.setPrefSize(2000, 2000);
