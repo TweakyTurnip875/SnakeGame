7c7e81f78c66e03bc4af3779def76d6b3aaeee53
TweakyTurnip875 <44481502+TweakyTurnip875@users.noreply.github.com>
2024-01-09 17:53:38 -0700

    Trigger proper game over when snake hits wall.

--- src/com/noah/main/Main.java
+++ src/com/noah/main/Main.java
@@ -83,25 +83,30 @@ public class Main extends Application {
 		this.direction = direction;
 	}
 	public void update() {
-		if(direction != DIRECTION_NONE) {
-			Cell nextCell = getNextCell(snake.getHead());
-			
-			if(nextCell.getCellType() == CellType.FOOD) {
-				snake.grow();
-				nextCell.setCellType(CellType.SNAKE_NODE);
-				isFood = false;
-			} else if(nextCell.getCellType() == CellType.SNAKE_NODE) {
-				isRunning = false;
-				snake.getHead().setStyle("-fx-background-color:red;");
+		try {
+			if(direction != DIRECTION_NONE) {
+				Cell nextCell = getNextCell(snake.getHead());
+				
+				if(nextCell.getCellType() == CellType.FOOD) {
+					snake.grow();
+					nextCell.setCellType(CellType.SNAKE_NODE);
+					isFood = false;
+				} else if(nextCell.getCellType() == CellType.SNAKE_NODE) {
+					isRunning = false;
+					snake.getHead().setStyle("-fx-background-color:red;");
+				}
+				snake.move(nextCell);
 			}
-			snake.move(nextCell);
-		}
-		int row = (int)(Math.random() * 19);
-		int col = (int)(Math.random() * 19);
-
-		if(!isFood && cells[col][row].getCellType() == CellType.EMPTY) {
-			cells[col][row].setCellType(CellType.FOOD);
-			isFood = true;
+			int row = (int)(Math.random() * 19);
+			int col = (int)(Math.random() * 19);
+	
+			if(!isFood && cells[col][row].getCellType() == CellType.EMPTY) {
+				cells[col][row].setCellType(CellType.FOOD);
+				isFood = true;
+			}
+		}  catch(ArrayIndexOutOfBoundsException ex) {
+			isRunning = false;
+			snake.getHead().setStyle("-fx-background-color: red;");
 		}
 	}
 	public Cell getNextCell(Cell currentLocation) {
